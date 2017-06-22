2796e2713a1e958e15a3ef38b68d8f9bffa5bf66
wangaimin <448497795@qq.com>
2017-06-22 09:41:08 +0800

    123

--- InsertResource/Form1.Designer.cs
+++ InsertResource/Form1.Designer.cs
@@ -28,12 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.tbResource = new System.Windows.Forms.TextBox();
+            this.btnSupplierCategory_Resource = new System.Windows.Forms.Button();
+            this.btnSystemCategory_Resource = new System.Windows.Forms.Button();
+            this.btnBidTool_TenderBidStatusItem_Resource = new System.Windows.Forms.Button();
+            this.btnSystemOrganization_Resource = new System.Windows.Forms.Button();
+            this.btnSystemArea_Resource = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbDBConfig = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(110, 138);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(191, 72);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "选择数据";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // tbResource
+            // 
+            this.tbResource.Location = new System.Drawing.Point(353, 138);
+            this.tbResource.Multiline = true;
+            this.tbResource.Name = "tbResource";
+            this.tbResource.Size = new System.Drawing.Size(858, 72);
+            this.tbResource.TabIndex = 1;
+            this.tbResource.Text = "C:\\Users\\admin\\Desktop\\DB翻译\\已翻译\\区域.xlsx";
+            // 
+            // btnSupplierCategory_Resource
+            // 
+            this.btnSupplierCategory_Resource.Location = new System.Drawing.Point(338, 301);
+            this.btnSupplierCategory_Resource.Name = "btnSupplierCategory_Resource";
+            this.btnSupplierCategory_Resource.Size = new System.Drawing.Size(191, 72);
+            this.btnSupplierCategory_Resource.TabIndex = 2;
+            this.btnSupplierCategory_Resource.Text = "导入分供方分类资源";
+            this.btnSupplierCategory_Resource.UseVisualStyleBackColor = true;
+            this.btnSupplierCategory_Resource.Click += new System.EventHandler(this.btnSupplierCategory_Resource_Click);
+            // 
+            // btnSystemCategory_Resource
+            // 
+            this.btnSystemCategory_Resource.Location = new System.Drawing.Point(110, 301);
+            this.btnSystemCategory_Resource.Name = "btnSystemCategory_Resource";
+            this.btnSystemCategory_Resource.Size = new System.Drawing.Size(191, 72);
+            this.btnSystemCategory_Resource.TabIndex = 3;
+            this.btnSystemCategory_Resource.Text = "导入品类资源";
+            this.btnSystemCategory_Resource.UseVisualStyleBackColor = true;
+            this.btnSystemCategory_Resource.Click += new System.EventHandler(this.btnSystemCategory_Resource_Click);
+            // 
+            // btnBidTool_TenderBidStatusItem_Resource
+            // 
+            this.btnBidTool_TenderBidStatusItem_Resource.Location = new System.Drawing.Point(553, 301);
+            this.btnBidTool_TenderBidStatusItem_Resource.Name = "btnBidTool_TenderBidStatusItem_Resource";
+            this.btnBidTool_TenderBidStatusItem_Resource.Size = new System.Drawing.Size(191, 72);
+            this.btnBidTool_TenderBidStatusItem_Resource.TabIndex = 4;
+            this.btnBidTool_TenderBidStatusItem_Resource.Text = "导入招标状态资源";
+            this.btnBidTool_TenderBidStatusItem_Resource.UseVisualStyleBackColor = true;
+            this.btnBidTool_TenderBidStatusItem_Resource.Click += new System.EventHandler(this.btnBidTool_TenderBidStatusItem_Resource_Click);
+            // 
+            // btnSystemOrganization_Resource
+            // 
+            this.btnSystemOrganization_Resource.Location = new System.Drawing.Point(774, 301);
+            this.btnSystemOrganization_Resource.Name = "btnSystemOrganization_Resource";
+            this.btnSystemOrganization_Resource.Size = new System.Drawing.Size(191, 72);
+            this.btnSystemOrganization_Resource.TabIndex = 5;
+            this.btnSystemOrganization_Resource.Text = "导入组织资源";
+            this.btnSystemOrganization_Resource.UseVisualStyleBackColor = true;
+            this.btnSystemOrganization_Resource.Click += new System.EventHandler(this.btnSystemOrganization_Resource_Click);
+            // 
+            // btnSystemArea_Resource
+            // 
+            this.btnSystemArea_Resource.Location = new System.Drawing.Point(994, 301);
+            this.btnSystemArea_Resource.Name = "btnSystemArea_Resource";
+            this.btnSystemArea_Resource.Size = new System.Drawing.Size(191, 72);
+            this.btnSystemArea_Resource.TabIndex = 6;
+            this.btnSystemArea_Resource.Text = "导入区域资源";
+            this.btnSystemArea_Resource.UseVisualStyleBackColor = true;
+            this.btnSystemArea_Resource.Click += new System.EventHandler(this.btnSystemArea_Resource_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(106, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 30);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "数据库地址：";
+            // 
+            // tbDBConfig
+            // 
+            this.tbDBConfig.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tbDBConfig.Location = new System.Drawing.Point(353, 28);
+            this.tbDBConfig.Name = "tbDBConfig";
+            this.tbDBConfig.Size = new System.Drawing.Size(858, 42);
+            this.tbDBConfig.TabIndex = 21;
+            this.tbDBConfig.Text = " data source=localhost;database=YZ_Operation\r\n;user id=sa;password=yzw@123;Timeou" +
+    "t=30;";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1297, 543);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbDBConfig);
+            this.Controls.Add(this.btnSystemArea_Resource);
+            this.Controls.Add(this.btnSystemOrganization_Resource);
+            this.Controls.Add(this.btnBidTool_TenderBidStatusItem_Resource);
+            this.Controls.Add(this.btnSystemCategory_Resource);
+            this.Controls.Add(this.btnSupplierCategory_Resource);
+            this.Controls.Add(this.tbResource);
+            this.Controls.Add(this.btnSelect);
+            this.Name = "Form1";
+            this.Text = "写入资源表";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.TextBox tbResource;
+        private System.Windows.Forms.Button btnSupplierCategory_Resource;
+        private System.Windows.Forms.Button btnSystemCategory_Resource;
+        private System.Windows.Forms.Button btnBidTool_TenderBidStatusItem_Resource;
+        private System.Windows.Forms.Button btnSystemOrganization_Resource;
+        private System.Windows.Forms.Button btnSystemArea_Resource;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbDBConfig;
     }
 }
 
--- InsertResource/Form1.cs
+++ InsertResource/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npoi.Mapper;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Dapper;
+using System.Data.SqlClient;
 
 namespace InsertResource
 {
@@ -15,6 +18,306 @@ namespace InsertResource
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSystemCategory_Resource_Click(object sender, EventArgs e)
+        {
+
+            string path = tbResource.Text.Trim();
+
+            if (tbDBConfig.Text.Trim() == "")
+            {
+                MessageBox.Show("数据库地址不能为空！");
+                return;
+            }
+            if (path.IndexOf(".xlsx") <= 0)
+            {
+                MessageBox.Show("请选择正确的excel");
+                return;
+            }
+
+
+            var mapper = new Mapper(path);
+            var resultData = mapper.Take<SystemCategory_Resource>(0);
+            List<SystemCategory_Resource> systemCategory_ResourceList = new List<SystemCategory_Resource>();
+            systemCategory_ResourceList.AddRange(resultData.Where(m => m.Value.SystemCategorySysNo != 0).Select(m => m.Value));
+            if (systemCategory_ResourceList.Count()==0)
+            {
+                MessageBox.Show("确定选择的是品类excel？");
+                return;
+            }
+
+            foreach (var item in systemCategory_ResourceList)
+            {
+                item.CategoryName = item.CategoryName.Trim();
+            }
+            string insertSql = @"INSERT INTO [YZ_Operation].[dbo].[SystemCategory_Resource]
+                                   ([SystemCategorySysNo]
+                                   ,[LanguageCode]
+                                   ,[CategoryName]
+                                   ,[InUserSysNo]
+                                   ,[InUserName]
+                                   ,[InDate]
+                                   ,[EditUserSysNo]
+                                   ,[EditUserName]
+                                   ,[EditDate])
+                             VALUES
+                                   (@SystemCategorySysNo
+                                   ,@LanguageCode
+                                   ,@CategoryName
+                                    ,0
+                                   ,'王爱民'
+                                   ,GetDate()
+                                   , 0
+                                   , '王爱民'
+                                   , GetDate())";
+            using (IDbConnection conn = new SqlConnection(tbDBConfig.Text.Trim()))
+            {
+                conn.Open();
+                conn.Execute(insertSql, systemCategory_ResourceList);
+            }
+            MessageBox.Show("成功");
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "(*.xlsx)|*.xlsx";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                tbResource.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void btnSupplierCategory_Resource_Click(object sender, EventArgs e)
+        {
+
+            string path = tbResource.Text.Trim();
+
+            if (tbDBConfig.Text.Trim() == "")
+            {
+                MessageBox.Show("数据库地址不能为空！");
+                return;
+            }
+            if (path.IndexOf(".xlsx") <= 0)
+            {
+                MessageBox.Show("请选择正确的excel");
+                return;
+            }
+
+
+            var mapper = new Mapper(path);
+            var resultData = mapper.Take<SupplierCategory_Resource>(0);
+            List<SupplierCategory_Resource> supplierCategory_Resource = new List<SupplierCategory_Resource>();
+            supplierCategory_Resource.AddRange(resultData.Where(m => m.Value.SupplierCategorySysNo != 0).Select(m => m.Value));
+            if (supplierCategory_Resource.Count() == 0)
+            {
+                MessageBox.Show("确定选择的是分供方分类excel？");
+                return;
+            }
+
+            foreach (var item in supplierCategory_Resource)
+            {
+                item.CategoryName = item.CategoryName.Trim();
+            }
+
+            string insertSql = @"INSERT INTO  [YZ_Supplier].[dbo].[SupplierCategory_Resource]
+                                   ([SupplierCategorySysNo]
+                                   ,[LanguageCode]
+                                   ,[CategoryName]
+                                   ,[InUserSysNo]
+                                   ,[InUserName]
+                                   ,[InDate]
+                                   ,[EditUserSysNo]
+                                   ,[EditUserName]
+                                   ,[EditDate])
+                             VALUES
+                                   (@SupplierCategorySysNo
+                                   ,@LanguageCode
+                                   ,@CategoryName
+                                    ,0
+                                   ,'王爱民'
+                                   ,GetDate()
+                                   , 0
+                                   , '王爱民'
+                                   , GetDate())";
+            using (IDbConnection conn = new SqlConnection(tbDBConfig.Text.Trim()))
+            {
+                conn.Open();
+                conn.Execute(insertSql, supplierCategory_Resource);
+            }
+            MessageBox.Show("成功");
+        }
+
+        private void btnBidTool_TenderBidStatusItem_Resource_Click(object sender, EventArgs e)
+        {
+            string path = tbResource.Text.Trim();
+
+            if (tbDBConfig.Text.Trim() == "")
+            {
+                MessageBox.Show("数据库地址不能为空！");
+                return;
+            }
+            if (path.IndexOf(".xlsx") <= 0)
+            {
+                MessageBox.Show("请选择正确的excel");
+                return;
+            }
+
+
+            var mapper = new Mapper(path);
+            var resultData = mapper.Take<BidTool_TenderBidStatusItem_Resource>(0);
+            List<BidTool_TenderBidStatusItem_Resource> bidTool_TenderBidStatusItem_Resource = new List<BidTool_TenderBidStatusItem_Resource>();
+            bidTool_TenderBidStatusItem_Resource.AddRange(resultData.Where(m => m.Value.BidTool_TenderBidStatusItemSysNo != 0).Select(m => m.Value));
+            if (bidTool_TenderBidStatusItem_Resource.Count() == 0)
+            {
+                MessageBox.Show("确定选择的是投标状态excel？");
+                return;
+            }
+            foreach (var item in bidTool_TenderBidStatusItem_Resource)
+            {
+                item.NoticeContent = item.NoticeContent.Trim();
+            }
+
+
+            string insertSql = @"INSERT INTO [YZ_Tender].[dbo].[BidTool_TenderBidStatusItem_Resource]
+                                   ([BidTool_TenderBidStatusItemSysNo]
+                                   ,[LanguageCode]
+                                   ,[NoticeContent]
+                                  )
+                             VALUES
+                                   (@BidTool_TenderBidStatusItemSysNo
+                                   ,@LanguageCode
+                                   ,@NoticeContent
+                                   )";
+            using (IDbConnection conn = new SqlConnection(tbDBConfig.Text.Trim()))
+            {
+                conn.Open();
+                conn.Execute(insertSql, bidTool_TenderBidStatusItem_Resource);
+            }
+            MessageBox.Show("成功");
+        }
+
+        private void btnSystemArea_Resource_Click(object sender, EventArgs e)
+        {
+            string path = tbResource.Text.Trim();
+
+            if (tbDBConfig.Text.Trim() == "")
+            {
+                MessageBox.Show("数据库地址不能为空！");
+                return;
+            }
+            if (path.IndexOf(".xlsx") <= 0)
+            {
+                MessageBox.Show("请选择正确的excel");
+                return;
+            }
+
+
+            var mapper = new Mapper(path);
+            var resultData = mapper.Take<SystemArea_Resource>(0);
+            List<SystemArea_Resource> systemArea_Resource = new List<SystemArea_Resource>();
+            systemArea_Resource.AddRange(resultData.Where(m => m.Value.SystemAreaSysNo != 0).Select(m => m.Value));
+            if (systemArea_Resource.Count() == 0)
+            {
+                MessageBox.Show("确定选择的是区域excel？");
+                return;
+            }
+
+            foreach (var item in systemArea_Resource)
+            {
+                item.AreaName = item.AreaName.Trim();
+            }
+
+            string insertSql = @"INSERT INTO YZ_Operation.[dbo].[SystemArea_Resource]
+                                   ([SystemAreaSysNo]
+                                   ,[LanguageCode]
+                                   ,[AreaName]
+                                   ,[InUserSysNo]
+                                   ,[InUserName]
+                                   ,[InDate]
+                                   ,[EditUserSysNo]
+                                   ,[EditUserName]
+                                   ,[EditDate])
+                             VALUES
+                                   ( @SystemAreaSysNo
+                                   ,@LanguageCode
+                                   , @AreaName
+                                   ,0
+                                   ,'王爱民'
+                                   ,GetDate()
+                                   , 0
+                                   , '王爱民'
+                                   , GetDate())";
+            using (IDbConnection conn = new SqlConnection(tbDBConfig.Text.Trim()))
+            {
+                conn.Open();
+                conn.Execute(insertSql, systemArea_Resource);
+            }
+            MessageBox.Show("成功");
+        }
+
+        private void btnSystemOrganization_Resource_Click(object sender, EventArgs e)
+        {
+            string path = tbResource.Text.Trim();
+
+            if (tbDBConfig.Text.Trim() == "")
+            {
+                MessageBox.Show("数据库地址不能为空！");
+                return;
+            }
+            if (path.IndexOf(".xlsx") <= 0)
+            {
+                MessageBox.Show("请选择正确的excel");
+                return;
+            }
+
+
+            var mapper = new Mapper(path);
+            var resultData = mapper.Take<SystemOrganization_Resource>(0);
+            List<SystemOrganization_Resource> systemOrganization_Resource = new List<SystemOrganization_Resource>();
+            systemOrganization_Resource.AddRange(resultData.Where(m => m.Value.SystemOrganizationSysNo != 0).Select(m => m.Value));
+            if (systemOrganization_Resource.Count() == 0)
+            {
+                MessageBox.Show("确定选择的是组织excel？");
+                return;
+            }
+
+            foreach (var item in systemOrganization_Resource)
+            {
+                item.OrganizationName = item.OrganizationName.Trim();
+                item.OrganizationFullName = item.OrganizationFullName.Trim();
+            }
+
+            string insertSql = @"INSERT INTO [YZ_AuthCenter].[dbo].[SystemOrganization_Resource]
+                                   ([SystemOrganizationSysNo]
+                                   ,[LanguageCode]
+                                   ,[OrganizationName]
+                                   ,[OrganizationFullName]
+                                   ,[InUserSysNo]
+                                   ,[InUserName]
+                                   ,[InDate]
+                                   ,[EditUserSysNo]
+                                   ,[EditUserName]
+                                   ,[EditDate])
+                             VALUES
+                                   (@SystemOrganizationSysNo
+                                   , @LanguageCode
+                                   , @OrganizationName
+                                   , @OrganizationFullName
+                                   , @InUserSysNo
+                                   , @InUserName
+                                   , @InDate
+                                   , @EditUserSysNo
+                                   , @EditUserName
+                                   , @EditDate)";
+            using (IDbConnection conn = new SqlConnection(tbDBConfig.Text.Trim()))
+            {
+                conn.Open();
+                conn.Execute(insertSql, systemOrganization_Resource);
+            }
+            MessageBox.Show("成功");
         }
     }
 }
